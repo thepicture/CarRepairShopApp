a086dbba3af677a1b9cfae42ea5bbd061619062d
Student46 <fuzzyeastern@protonmail.com>
2021-06-16 17:39:17 +0500

    Make a new order for customer working

--- View/CustomerWindow.xaml.cs
+++ View/CustomerWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CarRepairShopApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -15,7 +16,7 @@ namespace CarRepairShopApp.View
             InitializeComponent();
             ServicesList.ItemsSource = Manager.Context.Service.ToList();
             List<Auto> autos = Manager.Context.Auto.ToList();
-            autos.Insert(0, new Auto { A_NAME = "Все автомобили" });
+            autos.Insert(0, new Auto { A_NAME = "Не указано" });
             ComboCar.ItemsSource = autos;
         }
 
@@ -24,6 +25,7 @@ namespace CarRepairShopApp.View
             List<Service> services = Manager.Context.Service.ToList();
             if (ComboCar.SelectedIndex != 0)
             {
+
             }
             services = services.Where(s => s.SE_NAME.ToLower().Contains(TBoxServiceName.Text.ToLower())).ToList();
             ServicesList.ItemsSource = services;
@@ -43,6 +45,60 @@ namespace CarRepairShopApp.View
                 == MessageBoxResult.Yes)
             {
                 Close();
+            }
+        }
+
+        private void BtnMakeAnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            Order order = new Order();
+            if (Manager.Context.Client.Where(c => c.CL_NAME.Equals(Manager.CurrentUser.USER_NAME)).Any().Equals(false))
+            {
+                Client client = new Client
+                {
+                    CL_NAME = Manager.CurrentUser.USER_NAME,
+                };
+                Manager.Context.Client.Add(client);
+                Manager.Context.SaveChanges();
+            }
+            order.O_CREATEDATE = DateTime.Now;
+            Random random = new Random();
+            order.Master.Add(Manager.Context.Master.ToList().OrderBy(m => random.Next()).First());
+            order.Client.Add(Manager.Context.Client.Where(c => c.CL_NAME.Equals(Manager.CurrentUser.USER_NAME)).First());
+            order.Status = Manager.Context.Status.Where(s => s.ST_STATE.Contains("в обработке")).FirstOrDefault();
+            order.TypeOfCar = (ComboCar.SelectedItem as Auto).TypeOfCar.First();
+            foreach (Service service in ServicesList.SelectedItems)
+            {
+                order.Service.Add(service);
+            }
+            try
+            {
+                Manager.Context.Order.Add(order);
+                Manager.Context.SaveChanges();
+                MessageBox.Show("Заявка успешно подана!",
+                    "Успешно!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                ServicesList.SelectedItems.Clear();
+                BtnMakeAnOrder.Visibility = Visibility.Hidden;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Пожалуйста, попробуйте подать заявку ещё раз!",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void ServicesList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ComboCar.SelectedIndex != 0)
+            {
+                BtnMakeAnOrder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnMakeAnOrder.Visibility = Visibility.Hidden;
             }
         }
     }
