82cb4044402d58d34bc045b39a146289fc98f8cf
Student46 <fuzzyeastern@protonmail.com>
2021-06-16 21:36:41 +0500

    UI Improvements

---
+++ Model/TypeOfCarHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRepairShopApp.Model
+{
+    public partial class TypeOfCar
+    {
+        public string GetModelAndAutoName
+        {
+            get
+            {
+                return Auto.A_NAME + " " + T_NAME;
+            }
+        }
+    }
+}
