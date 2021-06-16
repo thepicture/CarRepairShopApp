a5241bdc09a649f726c0f2f6812710eded83b691
Student46 <fuzzyeastern@protonmail.com>
2021-06-16 11:55:55 +0500

    Add service sost column

---
+++ Model/ServiceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepairShopApp.Model
+{
+    public partial class Service
+    {
+        public string GetServicePrice
+        {
+            get
+            {
+                return ServiceOfModel.Sum(s => s.COST).ToString() + " руб.";
+            }
+        }
+    }
+}
